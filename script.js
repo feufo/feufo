038f3b24ee7b36492204e5cce42ecc2636bf1600
feufo <122990101+feufo@users.noreply.github.com>
2023-03-17 21:35:42 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -82,3 +82,13 @@ $('#carousel-example').on('slide.bs.carousel', function (e) {
   }
 });
 
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
